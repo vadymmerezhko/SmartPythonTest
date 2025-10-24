54ef8e10d29948c81588097f9f1979040a1b2dc8
vadymmerezhko <vadymm@gmail.com>
2025-10-23 19:59:37 -0700

    Data provider for positive tests added to login test. 0.1.3.

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,10 +1,19 @@
+import pytest
 from pages.inventory_page import InventoryPage
 from pages.login_page import LoginPage
 
-def test_login_with_valid_credentials(page):
+
+# Parametrize valid usernames and passwords
+@pytest.mark.parametrize("username,password", [
+    ("standard_user", "secret_sauce"),
+    ("visual_user", "secret_sauce"),
+    ("performance_glitch_user", "secret_sauce")
+])
+
+def test_login_with_valid_credentials(page, username, password):
     login_page = LoginPage(page)
     login_page.goto()
-    login_page.login("standard_user", "secret_sauce")
+    login_page.login(username, password)
 
     inventory_page = InventoryPage(page)
     inventory_page.assert_on_inventory_page()
